04b53100d35226d4faa7d394cafbca9184137922
MaryKwawu <may419316@gmail.com>
2023-05-18 12:40:39 +0000

    0. My privileges!

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
- script that creates the table unique_id on your MySQL server
+-- A script that creates the table unique_id on your MySQL server
 CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
